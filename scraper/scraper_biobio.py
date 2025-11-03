393fcc911c5d43ea384ff03f14c653f51f32869d
LeaffLovesBingus <mati.sotitol@gmail.com>
2025-11-03 13:05:51 -0300

    avance en el scrapper del medio biobio
    el script ya puede scrappear los 1400 artículos extraidos por el crawler de manera exitosa

--- scraper/scraper_biobio.py
+++ scraper/scraper_biobio.py
@@ -2,15 +2,27 @@ import requests                     # Descargar HTML
 import json                         # Parsear info
 from bs4 import BeautifulSoup       # Parsear HTML y extraer datos
 from urllib.parse import urljoin    # Transformar urls de imágenes
+import re
 
 
 def extract(soup: BeautifulSoup, selectors: list[str], default = None) -> str | None:
     for sel in selectors:
         info = soup.select_one(sel)
         if info:
-            txt = info.get_text(strip = True)
+            txt = info.get_text(strip = True, separator = " ")
             if txt: 
                 return txt
+    return default
+
+
+def extract_text_only(soup: BeautifulSoup, selectors: list[str], default = None) -> str | None:
+    for sel in selectors:
+        info = soup.select_one(sel)
+        if info:
+            txt = " ".join(info.find_all(string = True, recursive = False)).strip()
+            if txt:
+                return txt
+            
     return default
 
 
@@ -24,6 +36,12 @@ def extract_multimedia(soup: BeautifulSoup, selectors: list, default: str = "") 
                 img.get("data-src") or
                 img.get("data-lazy-src")
             )
+
+            if not src and img.has_attr("style"):
+                match = re.search(r'background-image\s*:\s*url\((.*?)\)', img["style"])
+                if match:
+                    src = match.group(1).strip(' "\'')
+
             if src and isinstance(src, str):
                 url = urljoin(default, src.strip())
                 images.add(url)
@@ -31,7 +49,21 @@ def extract_multimedia(soup: BeautifulSoup, selectors: list, default: str = "") 
     return list(images)
 
 
-def scrap_news_article(url: str, validate: bool = False) -> dict | None:
+def extract_body(soup: BeautifulSoup, selectors: list, default: str = "") -> str:
+    cuerpo = []
+    for sel in selectors:
+        for parrafo in soup.select(sel):
+            if parrafo:
+                txt = parrafo.get_text(strip = True, separator = " ")
+                cuerpo.append(txt) if txt else cuerpo.append(default)
+    
+    if not cuerpo:
+        return None
+    else:
+        return "\n".join(cuerpo)
+
+
+def scrap_news_article(url: str, *tags, validate: bool = False) -> dict | None:
     try:
         # Realizar una request al sitio
         response = requests.get(url, timeout = 10)
@@ -41,15 +73,24 @@ def scrap_news_article(url: str, validate: bool = False) -> dict | None:
         # Parsear y extraer respuesta
         soup = BeautifulSoup(response.text, "html.parser")
 
-        fecha = extract(soup, [
-            "div.post-date",
-            "div.autor-fecha-container p.fecha"
-        ])
+        fecha = (
+            extract(soup, [
+                "div.post-date",
+                "div.autor-fecha-container p.fecha",
+                "div.nota p.fecha",
+                "div.nota-top-content div.top-content-text p.fecha",
+            ])
+            or
+            extract_text_only(soup, [
+                "div.fecha-visitas p.fecha",
+            ])
+        )
         if validate and not fecha: return None
 
         titulo = extract(soup, [
             "h1.post-title",
-            "h1.titulo"
+            "h1.titulo",
+            "div.nota-top-content div.top-content-text h1.titular",
         ])
         if validate and not titulo: return None
 
@@ -57,43 +98,47 @@ def scrap_news_article(url: str, validate: bool = False) -> dict | None:
             "div.autores-trust-project div.contenedor-datos p.nombres a",
             "div.author div.creditos-nota div.autores span.autor b",
             "div.autor-opinion div.informacion a.nombre",
+            "div.autor div.creditos-nota div.autores span.autor b a",
+            "div.container-nota-body span.autor b a",
         ])
-        if validate and not autor: return None
 
         desc_autor = extract(soup, [
             "div.autores-trust-project div.contenedor-datos p.cargo",
-            "div.autor-opinion div.informacion p.cargo"
+            "div.autor-opinion div.informacion p.cargo",
         ])
 
         abstract = extract(soup, [
             "div.post-main div.post-content div.post-excerpt p",
+            "div.contenido-nota div.post-excerpt p"
         ])
 
-        # **Mover a su función propia para poder realizar validaciones
-        cuerpo = (
-            "\n".join([
-                p.get_text(strip = True) 
-                for p in soup.select("div.post-main div.post-content div.container-redes-contenido p")
-                if p.get_text(strip = True)
-            ])
-        )
+        cuerpo = extract_body(soup, [
+            "div.post-main div.post-content div.container-redes-contenido p, div.post-main div.post-content div.container-redes-contenido h2",
+            "div.container-redes-contenido div.contenido-nota h2, div.container-redes-contenido div.contenido-nota p",
+            "div.contenido-nota div[class^='banners-contenido-nota-'] h2, div.contenido-nota div[class^='banners-contenido-nota-'] p",
+            "div.container-nota-body div.nota-content div.contenido p, div.container-nota-body div.nota-content div.contenido h2",
+        ])
         if validate and not cuerpo: return None
 
         multimedia = extract_multimedia(soup, [
             "div.post-main div.post-image img",
-            "div.post-main div.post-content div.container-redes-contenido img"
+            "div.post-main div.post-content div.container-redes-contenido img",
+            "div.imagen",
+            "div.contenedor-imagen-titulo div.imagen img",
+            "div.nota-top-content img"
         ])
-        if validate and not multimedia: return None
 
         return {
             "url": url,
             "titulo": titulo,
             "fecha": fecha,
+            "tags": tags,
             "autor": autor,
             "desc_autor": desc_autor,
             "abstract": abstract,
             "cuerpo": cuerpo,
-            "multimedia": multimedia
+            "multimedia": multimedia,
+            "tipo_multimedia": "imagen"
         }
 
     except Exception as e:
@@ -102,7 +147,7 @@ def scrap_news_article(url: str, validate: bool = False) -> dict | None:
 
 
 def main():
-    test_url = "https://www.biobiochile.cl/noticias/dopamina/2025/10/30/una-humillacion-critican-eliminacion-de-nidyan-fabregat-de-fdb-y-apuntan-contra-vasco-moulian.shtml"
+    test_url = "https://www.biobiochile.cl/noticias/servicios/beneficios/2025/10/23/asi-funciona-el-beneficio-estrudiantil-que-cubre-mas-de-un-millon-de-pesos-del-arancel.shtml"
     noticia = scrap_news_article(test_url)
     if noticia:
         print(json.dumps(noticia, indent = 3, ensure_ascii = False))
@@ -114,6 +159,12 @@ def main():
 # 3. "https://www.biobiochile.cl/noticias/artes-y-cultura/actualidad-cultural/2025/09/25/11-dias-de-actividades-trae-bienal-de-arquitectura-y-programa-enfocado-en-revitalizacion-de-la-ciudad.shtml"
 # 4. "https://www.biobiochile.cl/noticias/servicios/explicado/2024/11/13/los-hombres-lo-desean-mas-que-las-mujeres-5-mitos-sobre-el-sexo-explicado-por-expertos.shtml"
 # 5. "https://www.biobiochile.cl/noticias/servicios/toma-nota/2025/03/16/viajar-y-comer-por-chile-los-menus-imperdibles-a-lo-largo-del-pais-segun-nueva-guia-gastronomica.shtml"
+# 6. "https://www.biobiochile.cl/noticias/dopamina/2025/10/30/una-humillacion-critican-eliminacion-de-nidyan-fabregat-de-fdb-y-apuntan-contra-vasco-moulian.shtml"
+# 7. "https://www.biobiochile.cl/especial/bio-bio-tuercas/noticias/2025/10/31/massa-califica-el-crashgate-como-el-mayor-escandalo-en-la-historia-del-deporte-reclama-titulo-de-f1.shtml"
+# 8. "https://www.biobiochile.cl/especial/bbcl-investiga/noticias/entrevistas/2025/11/01/falta-una-izquierda-mas-de-resultados-que-de-eslogan-carlos-cuadrado-ppd-candidato-a-diputado.shtml"
+# 9. "https://www.biobiochile.cl/especial/bbcl-investiga/noticias/de-pasillo/2025/10/30/embargan-bienes-de-alvaro-saieh-por-deuda-de-27-millones-de-dolares-con-banco-itau.shtml"
+# 10. "https://www.biobiochile.cl/noticias/servicios/beneficios/2025/10/23/asi-funciona-el-beneficio-estrudiantil-que-cubre-mas-de-un-millon-de-pesos-del-arancel.shtml"
+
 
 if __name__ == "__main__":
     main()
--- test_scraper.py
+++ test_scraper.py
@@ -1,19 +1,29 @@
 from scraper.scraper_biobio import scrap_news_article
 import csv
 
-def main():
+def main(desde: str = 1):
     with open("Crawler/biobiochile.csv", "r", newline = "", encoding = "utf-8") as f:
         reader = csv.reader(f)
         lista_url = [linea[0] for linea in reader if linea]
 
     for index, url in enumerate(lista_url, start = 1):
+        if index < desde:
+            print(f"Saltando url n° {index}")
+            continue
         doc = scrap_news_article(url, validate = True)
         if (isinstance(doc, dict)):
             print(f"Url {index} scrappeada exitosamente")
         else:
             print(f"Error al scrappear la url n° {index}") 
-            break           
+            break
 
 
 if __name__ == "__main__":
     main()
+
+'''
+Enviar 3 tipos de mensajes:
+- Mensajes de éxito cuando se logró scrappear un medio
+- Mensajes de falla cuando no se logró scrappear un medio
+- Datos scrappeados
+'''
